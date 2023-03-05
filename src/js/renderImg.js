932245a43a1d729e2c498c08b12107d30665417c
Inna Pokydko <inna.pokydko@gmail.com>
2023-03-05 19:21:31 +0200

    gallery

---
+++ src/js/renderImg.js
@@ -0,0 +1,27 @@
+export { renderImg }
+
+const gallery = document.querySelector(".gallery");
+
+function renderImg(images) {
+    const markup = images
+      .map(image => {
+        const { id, largeImageURL, webformatURL, tags, likes, views, comments, downloads } = image;
+        return `
+          <a class="photo-link" href="${largeImageURL}">
+            <div class="gphoto-card" id="${id}">
+              <img class="photo-card__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+              <div class="info">
+                <p class="info-item"><b>Likes</b>${likes}</p>
+                <p class="info-item"><b>Views</b>${views}</p>
+                <p class="info-item"><b>Comments</b>${comments}</p>
+                <p class="info-item"><b>Downloads</b>${downloads}</p>
+              </div>
+            </div>
+          </a>
+        `;
+      })
+      .join('');
+  
+    gallery.insertAdjacentHTML('beforeend', markup);
+  }
+
