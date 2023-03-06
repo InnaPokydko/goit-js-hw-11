05a38b6f1003f0cf36440054ff0176ab080de884
Inna Pokydko <inna.pokydko@gmail.com>
2023-03-06 20:07:20 +0200

    import to html

--- src/js/fetchImg.js
+++ src/js/fetchImg.js
@@ -1,1 +1,16 @@
 import axios from "axios"
+{ fatchImg }
+
+BASE_URL = 'https://pixabay.com/api/';
+const KEY = '34168491-a08a19ec58377d1b70d25ff83';
+
+function fatchImg() {
+    return fetch(`${BASE_URL}/`).then(
+      (response) => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      }
+    );
+  }
