0a40809cfe39e12a58b312ba65b57fadce2d2190
Inna Pokydko <inna.pokydko@gmail.com>
2023-03-07 20:13:13 +0200

    асинхр функція

--- src/js/fetchImg.js
+++ src/js/fetchImg.js
@@ -4,13 +4,21 @@ import axios from "axios"
 BASE_URL = 'https://pixabay.com/api/';
 const KEY = '34168491-a08a19ec58377d1b70d25ff83';
 
-function fatchImg() {
-    return fetch(`${BASE_URL}/`).then(
-      (response) => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      }
-    );
-  }
+// function fatchImg() {
+//     return fetch(`${BASE_URL}/`).then(
+//       (response) => {
+//         if (!response.ok) {
+//           throw new Error(response.status);
+//         }
+//         return response.json();
+//       }
+//     );
+//   }
+
+  const fatchImg = async () => {
+    const response = await fetch(`${BASE_URL}?KEY`);
+    const imgs = await response.json();
+    return imgs;
+  };
+  
+  fatchImg().then(imgs => console.log(imgs));
